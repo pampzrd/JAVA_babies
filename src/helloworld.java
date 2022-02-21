ebc71099b38a5086e75243b7f22b465b90a34496
pierrete <akahime2014@gmail.com>
2022-02-21 00:04:37 -0300

    upload

---
+++ src/helloworld.java
@@ -0,0 +1,43 @@
+public class helloworld {
+    public static void main(String[] args) {
+        //String[] é uma array vazia de texto e args é uma variável. Pode alterar o nome.
+        System.out.println("Olá, mundo!");
+        //esta linha imprimirá a soma de 2+2
+        //e este é um comentário
+        //métodos são instruções para o programa executar.
+        System.out.println(2+2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/length.java
@@ -0,0 +1,15 @@
+public class length {
+    public static void main(String[] txt)
+    {
+        /* Declara as variaveis String NOME = "VALOR"
+        * Depois pede para imprimir a variável junto com a classe .len gth() */
+
+        String a = "Primeira Budega";
+        String b = "Pica-pau mora na árvore";
+        String c = "Chapolin";
+        System.out.println("1 "+a+ "\n"+"2 "+b+"\n"+"3 "+c);
+        System.out.println(a.length());
+        System.out.println(b.length());
+        System.out.println(c.length());
+    }
+}
